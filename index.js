acdd8b93f8c767172672bb2b479ba9aa003f51e6
Sushant.sinha_94 <sushant1058jan@gmail.com>
2023-02-17 12:01:53 +0530

    keypress implemented

--- index.js
+++ index.js
@@ -2,9 +2,51 @@ no_of_drum_button=document.querySelectorAll(".drum").length; /**find the length 
 
 for (var i=0; i<no_of_drum_button; i++){
 
-document.querySelectorAll(".drum")[i].addEventListener("click", handleclick)  /**finds first button in document and add event listener */
+document.querySelectorAll(".drum")[i].addEventListener("click", function (){   /**finds first button in document and add event listener */
 
-function handleclick(){
-    alert("got clicked")
+    var buttonInnerHtml=this.innerHTML;
+    makesound(buttonInnerHtml)
+})  
 }
+
+document.addEventListener("keydown", function(event){
+    makesound(event.key)
+})
+
+function makesound(key){
+    switch (key){
+        case "w":
+            var tom1 = new Audio('sounds/tom-1.mp3')
+            tom1.play();
+        break;
+        case "a":
+            var tom2 = new Audio('sounds/tom-2.mp3')
+            tom2.play();
+        break;
+        case "s":
+            var tom3 = new Audio('sounds/tom-3.mp3')
+            tom3.play();
+        break;
+        case "d":
+            var tom4 = new Audio('sounds/tom-4.mp3')
+            tom4.play();
+        break;
+        case "j":
+            var snare = new Audio('sounds/snare.mp3')
+            snare.play();
+        break;
+        case "k":
+            var crash = new Audio('sounds/crash.mp3')
+            crash.play();
+        break;
+        case "l":
+            var kickbass = new Audio('sounds/kick-bass.mp3')
+            kickbass.play();
+        break;
+
+        default: console.log();
+
+    }
 }
+// var audio= new Audio("sounds/tom-1.mp3");
+// audio.play();
